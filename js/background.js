902300c54a8aecab8ff3f6028eaf8e71cc3c6ed8
Guai Zheng <37161400+gdotzheng@users.noreply.github.com>
2019-02-14 00:45:39 -0500

    Initial commit

---
+++ js/background.js
@@ -0,0 +1,18 @@
+var i = 0
+
+function createDiv() {
+  var button = document.createElement('div')
+  button.id = "addbutton"
+  button.innerHTML = '<img src="https://i.imgur.com/tJGKRxW.png" align="middle" width="100" height="100" onclick="createDiv();" />'
+  i += 1
+  if (i > 6) {
+    i = 0
+  }
+  var arr = ["box1", "box2", "box3", "box4", "box5", "box6", "box7"]
+  var div = document.createElement('div');
+  div.innerHTML = document.getElementById("box1").innerHTML
+  div.id = arr[i];
+  document.getElementById('display').removeChild(document.getElementById("addbutton"));
+  document.getElementById('display').appendChild(div);
+  document.getElementById('display').appendChild(button);
+}
