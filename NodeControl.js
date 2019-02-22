9ac3257df559fb68bbfd63f196eb1d9ba3306abb
Guai Zheng <37161400+gdotzheng@users.noreply.github.com>
2019-02-21 19:41:15 -0500

    Implemented nodejs

---
+++ NodeControl.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var request = require('request');
+var bodyparser = require('body-parser');
+
+var app = express();
+var path = require("path");
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json())
+
+app.get('/',function(req,res){//Starts Server from html file
+  res.sendFile(path.join(__dirname+'/travelcompanion.html'));
+});
+app.use(express.static('.'));
+
+app.get('/weather', function(req,res){
+  var URL = "https://api.aerisapi.com/forecasts/"+ req.query.city + "?&format=json&filter=day&limit="+req.query.day+"&fields=.weatherPrimary,periods.avgTempF,periods.icon,periods.pop,periods.weather&client_id=KSCDpQX8QB3nS5miKzeN4&client_secret=X5jy1n9EDwpMA3j02v1wadma59bHM3rR5qRykTyz";
+  request.get(URL,function(error,response,body){
+    var respond = JSON.parse(body)
+    var output = respond.response[0].periods[respond.response[0].periods.length-1]
+    //var output = [respond.response[0].periods[respond.response[0].periods.length-1].avgTempF,respond.response[0].periods[respond.response[0].periods.length-1].icon,respond.response[0].periods[respond.response[0].periods.length-1].pop,respond.response[0].periods[respond.response[0].periods.length-1].weather]
+    return res.send(JSON.stringify(output))
+	});
+})
+
+app.listen(8080, function(){
+  console.log("Server started")
+})
--- js/background.js
+++ js/background.js
@@ -1,65 +1,101 @@
 var i = 0
-var z = -1
+
+//$(document).ready(function(){
+  //$("#citysearch").change(doGeocode)
+//})
+function doGeocode() {
+  var addr = document.getElementById("citysearch");
+  // Get geocoder instance
+  var geocoder = new google.maps.Geocoder();
+
+  // Geocode the address
+  geocoder.geocode({
+    'address': addr.value
+  }, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+
+      // set it to the correct, formatted address if it's valid
+      addr.value = results[0].formatted_address
+      document.getElementById("d").style.display="block";
+
+      // show an error if it's not
+    } else alert("Invalid address");
+  });
+};
 
 function createDiv() {
   var button = document.createElement('div')
   button.id = "addbutton"
   button.innerHTML = '<img src="https://i.imgur.com/tJGKRxW.png" align="middle" width="100" height="100" onclick="createDiv();" />'
   i += 1
-  z += 1
   if (i > 6) {
     i = 0
   }
   var arr = ["box1", "box2", "box3", "box4", "box5", "box6", "box7"]
   var div = document.createElement('div');
-  div.innerHTML = "
-  <input class="addresses" id="citysearch" type="text" style="width: 150px; height: 30px" placeholder="Enter City">
-  <div id="result">
-    <div id="date">
+  div.innerHTML = `
+      <input class="addresses" id="citysearch" type="text" style="width: 150px; height: 30px" placeholder="Enter City" onchange="doGeocode()">
 
-    </div>
-    <div id="weather">
+      <div id="result">
+        <div id="date">
+          <center>
+              <input id="d" type="date" min="0000-00-00" style="width: 140px; height: 20px; text-align: center; display: none" onchange="getWeather()">
+          </center>
+        </div>
+        <div id="weather">
 
-    </div>
-    <div id="weathericon">
+        </div>
+        <div id="weathericon">
 
-    </div>
-    <div id="temp">
+        </div>
+        <div id="temp">
 
-    </div>
-  </div>";
+        </div>
+      </div>
+    </div>`;
   //div.innerHTML = document.getElementById("box1").innerHTML
   div.id = arr[i];
   document.getElementById('display').removeChild(document.getElementById("addbutton"));
   document.getElementById('display').appendChild(div);
   document.getElementById('display').appendChild(button);
   initialize()
-  var city = $("#" + arr[z]).find('#citysearch').val();
-  console.log(city)
-  getWeather(city, arr)
 }
 
-function getWeather(city, arr) {
+function getDateDiff(firstDate,secondDate){
+  var oneDay = 24*60*60*1000;
+  d1 = new Date(firstDate)
+  d2 = new Date(secondDate)
+  d2.setDate(d2.getDate()+2)
+  var datediff = Math.round(Math.abs((d1.getTime() - d2.getTime())/(oneDay)))
+  return datediff
+}
 
+function getWeather() {
+  var arr = ["box1", "box2", "box3", "box4", "box5", "box6", "box7"]
+  var city = $("#" + arr[i]).find('#citysearch').val();
+  var day = getDateDiff(new Date(),document.getElementById("d").value)
   //var url = "https://api.aerisapi.com/forecasts/"+ city +"?&format=json&filter=day&limit=14&client_id=KSCDpQX8QB3nS5miKzeN4&client_secret=X5jy1n9EDwpMA3j02v1wadma59bHM3rR5qRykTyz"
-  var url =  "https://api.aerisapi.com/forecasts/"+ city + "?&format=json&filter=day&limit=14&fields=.weatherPrimary,periods.avgTempF,periods.icon,periods.pop,periods.weather&client_id=KSCDpQX8QB3nS5miKzeN4&client_secret=X5jy1n9EDwpMA3j02v1wadma59bHM3rR5qRykTyz"
+  //var url =  "https://api.aerisapi.com/forecasts/"+ city + "?&format=json&filter=day&limit=14&fields=.weatherPrimary,periods.avgTempF,periods.icon,periods.pop,periods.weather&client_id=KSCDpQX8QB3nS5miKzeN4&client_secret=X5jy1n9EDwpMA3j02v1wadma59bHM3rR5qRykTyz"
+  var url = "/weather"
   $.ajax({
     type: "GET",
     url: url,
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
+    data: {
+      city:city,
+      day:day,
+    },
+    dataType: "text",
     success: function(msg) {
-
-      //var json = msg;
-      var weatherPrimary = msg.response[0].periods[i-1].weather
-      //var weatherIcon = msg.repsonse[0].periods[i-1].icon
+      var arr = JSON.parse(msg)
+      var weatherPrimary = arr.weather
+      var weatherIcon = arr.icon
       //console.log(weatherIcon)
-      var averageTemp = msg.response[0].periods[i-1].avgTempF
+      var averageTemp = arr.avgTempF
 
-      $("#" + arr[z]).find('#weather').html(weatherPrimary)
-      //$("#" + arr[z]).find('#weathericon').html(weathericon)
-      //$("#" + arr[z]).find("#weathericon").append('<img id="theImg" src="img/chancetstorm.png" />')
-      $("#" + arr[z]).find('#temp').html(averageTemp+" °F")
+      $("#" + arr[i]).find('#weather').html(weatherPrimary)
+      $("#" + arr[i]).find('#weathericon').html(weathericon)
+      $("#" + arr[i]).find("#weathericon").append('<img id="theImg" src="img/chancetstorm.png" />')
+      $("#" + arr[i]).find('#temp').html(averageTemp+" °F")
 
       //document.getElementById("weather").innerHTML = weatherPrimary
       //document.getElementById("temp").innerHTML = averageTemp
